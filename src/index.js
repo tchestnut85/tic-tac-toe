3992a19f31f211450955ea9363f4fa5cab26f362
tchestnut85 <tchestnut85@gmail.com>
2020-12-28 00:12:53 -0500

    removed serviceworker code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/App';
-import * as serviceWorker from './serviceWorker';
-
 
 ReactDOM.render(
     <React.StrictMode>
@@ -11,5 +9,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-serviceWorker.unregister();
